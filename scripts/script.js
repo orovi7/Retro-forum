ba604c38f0ca7872758850daa2464b8dbe48c933
orovi7 <rubaiyatahamimorovi@gmail.com>
2025-06-22 12:17:36 +0600

    just finished making the navbar

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+
+/**
+ * API
+LatestPosts: - https://openapi.programming-hero.com/api/retro-forum/latest-posts
+
+AllPosts: - https://openapi.programming-hero.com/api/retro-forum/posts
+
+PostSearchByQuery
+PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=categoryName
+
+Example
+PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=coding
+ */
+
+console.log('yea retro forum js has been connected')
